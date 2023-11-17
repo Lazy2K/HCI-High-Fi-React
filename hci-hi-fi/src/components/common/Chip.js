2387ba0fd366e9878e6634844596eb76eafcf663
Douglas <kekblob@gmail.com>
2023-11-17 17:05:20 +0000

    get gud

---
+++ hci-hi-fi/src/components/common/Chip.js
@@ -0,0 +1,9 @@
+export default function BirdScreen(props) {
+  return <div style={styles.chip}>Bird Type</div>;
+}
+
+const styles = {
+  chip: {
+    padding: "1rem",
+  },
+};
--- hci-hi-fi/src/components/screens/HomeScreen.js
+++ hci-hi-fi/src/components/screens/HomeScreen.js
@@ -1,7 +1,46 @@
+import Chip from "./../common/Chip";
+import Card from "./../common/Card";
+
 export default function HomeScreen(props) {
   return (
-    <div className="HomeScreen">
-      <div>Homescreen</div>
+    <div className="HomeScreen" style={styles.homeScreen}>
+      <div className="HomeScreenContainer" style={styles.homeContainer}>
+        <div className="ChipContainer" style={styles.chipContainer}>
+          <Chip />
+          <Chip />
+          <Chip />
+          <Chip />
+          <Chip />
+          <Chip />
+          <Chip />
+          <Chip />
+          <Chip />
+          <Chip />
+          <Chip />
+          <Chip />
+          <Chip />
+          <Chip />
+        </div>
+        <div className="CardContainer"></div>
+      </div>
     </div>
   );
 }
+
+const styles = {
+  homeScreen: {
+    width: "100%",
+    height: "100%",
+  },
+  homeContainer: {
+    padding: "2rem",
+    display: "flex",
+    flexDirection: "column",
+  },
+  chipContainer: {
+    display: "flex",
+    flexDirection: "row",
+    overflowX: "auto",
+    position: "absoulte",
+  },
+};
