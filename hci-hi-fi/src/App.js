ac3344f18b4e0e34ff4ab2d99d2bbca8ab73ef9c
Douglas <kekblob@gmail.com>
2023-11-17 13:25:24 +0000

    In progress css

--- hci-hi-fi/src/App.js
+++ hci-hi-fi/src/App.js
@@ -1,11 +1,17 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 // Applicatoin navigation homerow
 import Homerow from "./components/homerow/Homerow";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="screenContainer">
+        <div className="pageContainer"></div>
+        <div className="homerowContainer"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- hci-hi-fi/src/components/homerow/Homerow.js
+++ hci-hi-fi/src/components/homerow/Homerow.js
@@ -1,3 +1,21 @@
 export default function Homerow(props) {
-  return <p>HOMEROW</p>;
+  return (
+    <div className="homerowContainer" style={styles.homerowContainer}>
+      <ul>
+        <li>Hello</li>
+        <li>There</li>
+      </ul>
+    </div>
+  );
 }
+
+const styles = {
+  homerowContainer: {
+    width: "100vw",
+    height: "auto",
+    backgroundColor: "#ffffff",
+    textAlign: "center",
+    display: "flex",
+    justifyContent: "center",
+  },
+};
