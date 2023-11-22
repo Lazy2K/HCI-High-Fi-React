da1c0e9e7e7c5324acb1f505815929c6ba6f6f45
Douglas <kekblob@gmail.com>
2023-11-22 16:19:26 +0000

    Stuff

--- hci-hi-fi/src/components/screens/SettingsScreen.js
+++ hci-hi-fi/src/components/screens/SettingsScreen.js
@@ -1,3 +1,28 @@
+import BackButton from "../common/BackButton";
+
 export default function SettingsScreen(props) {
-  return <div>Settings</div>;
+  return (
+    <div className="SettingsScreen" style={styles.settingsScreen}>
+      <div className="HeaderContainer" style={styles.headerContainer}>
+        <BackButton />
+      </div>
+      <div className="PageContainer"></div>
+    </div>
+  );
 }
+
+const styles = {
+  settingsScreen: {
+    width: "100%",
+    height: "100%",
+    backdropFilter: "blur(5px)",
+    display: "flex",
+    flexDirection: "column",
+  },
+  headerContainer: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    padding: "2rem 2rem 2rem 2rem",
+  },
+};
