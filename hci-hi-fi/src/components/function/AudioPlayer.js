f8c099a7822e9ff65dffe2b157fd68d0d3ea17ac
Douglas <kekblob@gmail.com>
2023-11-24 21:43:30 +0000

    Omds this is more difficult than it needs to be

--- hci-hi-fi/src/components/function/AudioPlayer.js
+++ hci-hi-fi/src/components/function/AudioPlayer.js
@@ -6,17 +6,22 @@ export default function Sound(audioFile) {
   const audio = new Audio(FalconAudio); // This is hardcoded rn bc
   audio.loop = false;
 
+  const [isPaused, setIsPaused] = useState(false);
+
   const handlePlay = () => {
     audio.play();
+    setIsPaused(false);
   };
 
   const handlePause = () => {
     audio.pause();
+    setIsPaused(true);
   };
 
   const handleStop = () => {
     audio.pause();
     audio.currentTime = 0;
+    setIsPaused(false);
   };
 
   return (
--- hci-hi-fi/src/components/screens/BirdScreen.js
+++ hci-hi-fi/src/components/screens/BirdScreen.js
@@ -1,17 +1,19 @@
 import Sound from "../function/AudioPlayer";
 
-const FalconAudio =
-  "http://localhost:3000/static/media/falcontext.5912db496e7341dd729b.mp3";
-
-const runnnit = () => {
-  console.log(process.env.PUBLIC_URL + "/sounds/falcontext.mp3");
-};
-
 export default function BirdScreen(props) {
   return (
     <div>
-      <button onClick={runnnit}>Click</button>
-      <Sound audioFile={FalconAudio} />
+      <Sound audioFile={null} />
+      <div>
+        Falcons are birds of prey in the genus Falco, which includes about 40
+        species. Falcons are widely distributed on all continents of the world
+        except Antarctica. Adult falcons have thin, tapered wings, which enable
+        them to fly at high speed and change direction rapidly. Fledgling
+        falcons, in their first year of flying, have longer flight feathers,
+        which make their configuration more like that of a general-purpose bird
+        such as a broad wing. This makes flying easier while learning the
+        exceptional skills required to be effective hunters as adults.
+      </div>
     </div>
   );
 }
