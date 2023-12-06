49c665141e318920ab0a3b19ee92215a18479f78
Douglas <kekblob@gmail.com>
2023-12-06 17:21:55 +0000

    Account Screen

--- hci-hi-fi/src/components/screens/AccountScreen.js
+++ hci-hi-fi/src/components/screens/AccountScreen.js
@@ -1,16 +1,27 @@
 import SettingsScreen from "./SettingsScreen";
 
+import "./css/accountScreen.css";
+
+import profilePicture from "./../../assets/md/woman.jpg";
+import settingsIcon from "./../../assets/icons/Settings.png";
+
 export default function AccountScreen({ onSetRenderScreen }) {
   return (
-    <div className="HomeScreen">
-      <div>Account</div>
-      <button
-        onClick={() =>
-          onSetRenderScreen(<SettingsScreen clicky={onSetRenderScreen} />)
-        }
-      >
-        Settings
-      </button>
+    <div className="accountScreen">
+      <div className="accountScreenContainer">
+        <div className="screenHeader">
+          <img className="profilePicture" src={profilePicture} />
+          <button
+            className="settingsButton"
+            onClick={() =>
+              onSetRenderScreen(<SettingsScreen clicky={onSetRenderScreen} />)
+            }
+          >
+            <img class="settingsIcon" src={settingsIcon} />
+          </button>
+        </div>
+        <div className="accountTitle">Nick! Welcome to your account.</div>
+      </div>
     </div>
   );
 }
